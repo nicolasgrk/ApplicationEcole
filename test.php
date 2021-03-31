de8a00a8d6f2cd808f0908d03e886aec73ddc474
nicolasgrk <nicolasgurak@gmail.com>
2021-04-01 00:06:28 +0200

    tchat

--- test.php
+++ test.php
@@ -2,118 +2,43 @@
 include ( "include/_inc_parametres.php");
 include ( "include/_inc_connexion.php");
 header( 'content-type: text/html; charset=utf-8' );
+
+
 ?>
 
-
-     <html>
+<html>
 <head>
-    <title>Mon super emploi du temps</title>
-    <style type="text/css">
-        caption /* Titre du tableau */
-        {
-           margin: auto; /* Centre le titre du tableau */
-           font-family: Arial, Times, "Times New Roman", serif;
-           font-weight: bold;
-           font-size: 1.2em;
-           color: #009900;
-           margin-bottom: 20px; /* Pour éviter que le titre ne soit trop collé au tableau en-dessous */
-        }
- 
-        table /* Le tableau en lui-même */
-        {
-           margin: auto; /* Centre le tableau */
-           border: 4px outset green; /* Bordure du tableau avec effet 3D (outset) */
-           border-collapse: collapse; /* Colle les bordures entre elles */
-           width:100%;
-        }
-        th /* Les cellules d'en-tête */
-        {
-           background-color: #006600;
-           color: white;
-           font-size: 1.1em;
-           font-family: Arial, "Arial Black", Times, "Times New Roman", serif;
-           border:1px solid red;
-        }
- 
-        td /* Les cellules normales */
-        {
-           border: 1px solid black;
-           font-family: "Comic Sans MS", "Trebuchet MS", Times, "Times New Roman", serif;
-           text-align: center; /* Tous les textes des cellules seront centrés*/
-           padding: 5px; /* Petite marge intérieure aux cellules pour éviter que le texte touche les bordures */
-        }
-        td.time
-        {
-            width:5%;
-        }
-    </style>
- 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <title>Mon chat</title>
 </head>
+
 <body>
-<table>
-<tr>
-    <td>Jour</td>
-    <td>Heure</td>
-    <td>Matières</td>
-    <td>Salle</td>
-</tr>
-<?php
+    <section class="container">
+          <main class="row">
+          <div class="col-12 my-1">
+               <div class="p-2" id="discussion">
+               </div>
+          </div>
+          <div class="col-12 saisie">
+               <div class="input-group">
+                    <input type="text" class="form-control" id="texte" placeholder="Entrez votre texte">
+                    <div class="input-group-append">
+                         <span class="input-group-text" id="valid"><i class="la la-check"></i></span>
+                    </div>
+               </div>
+          </div>
 
-
-$infoEcole=$cnx->query('SELECT HOUR(date) as heure, DAYOFWEEK(date) as jour, date(date) as datedujour, matiere, salle FROM emploiDuTemps WHERE week(date) = week(CURRENT_DATE) and formation = 2 ORDER by date;'); 
-$infoEcole->setFetchMode(PDO::FETCH_OBJ);
-$prodInfo=$infoEcole->fetch();
-
-
-while($prodInfo)
-{ 
-
-          $dateMySQL = ($prodInfo->datedujour);
-          setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
-      
-?>
-
-
-  <tr>
-    <td><?php echo strftime("%A %d %B %Y",strtotime($dateMySQL))?> </td>
-    <td><?php echo $prodInfo->heure?></td>
-    <td><?php echo $prodInfo->matiere?></td>
-    <td><?php echo $prodInfo->salle?></td>
-  </tr>
-<?php
-$prodInfo=$infoEcole->fetch(); 
-
-}
-
-
-            
-?>
-
-
-</table>
-
-
-
-<table>
-
-<tr>
-     <td> Lundi</td>
-</tr>
-<tr>
-     <td> Mardi</td>
-</tr>
-<tr>
-     <td> Mercredi</td>
-</tr>
-<tr>
-     <td> Jeudi</td>
-</tr>
-<tr>
-     <td> Vendredi</td>
-</tr>
-
-</table>
-
+        </main>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
 </body>
 
+</html>
 
